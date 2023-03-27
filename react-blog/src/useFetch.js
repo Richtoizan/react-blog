8879d1f2398a6a15ae9e64d3765b38d537efb98c
Zaid Khirfan <Khirfan@hotmail.com>
2023-03-28 02:06:12 +0300

    Added create form using controlled inputs and added an info page for each blog

--- react-blog/src/useFetch.js
+++ react-blog/src/useFetch.js
@@ -7,7 +7,10 @@ const useFetch = (url) =>{
     const [error, setError] = useState(null);
 
     useEffect(() => {
-        fetch(url)
+
+        const abortCont = new AbortController();
+
+        fetch(url, {signal: abortCont.signal})
             .then(res => {
                 if(!res.ok){
                     throw Error('Could not fetch the data!');
@@ -20,9 +23,14 @@ const useFetch = (url) =>{
                 setError(null);
             })
             .catch(err=> {
+                if(err==='AbortError'){
+                    console.log('Fetch aborted!');
+                } else {
                 setError(err.message);
                 setIsPending(false);
-            })
+            }});
+
+        return () => abortCont.abort();
     }, [url]);
 
     return {data, isPending, error};
